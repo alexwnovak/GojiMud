732429611f216a4762086c0e28015afd1c4dd042
Alex Novak <alexwnovak@gmail.com>
2014-02-05 13:02:22 -0500

    Launcher now instantiates/runs a MudApplication.

--- Src/GojiMud/GojiMud.Launcher/Program.cs
+++ Src/GojiMud/GojiMud.Launcher/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace GojiMud.Launcher
+﻿using GojiMud.Engine;
+
+namespace GojiMud.Launcher
 {
    internal static class Program
    {
       private static void Main( string[] arguments )
       {
+         var mudApplication = new MudApplication();
+
+         mudApplication.Run( arguments );
       }
    }
 }
