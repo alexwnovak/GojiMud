942e2d26caf4eef9e52781eec5dd9f4c4a19a565
Alex Novak <alexwnovak@gmail.com>
2014-02-05 13:28:18 -0500

    MudApplication now creates the unity container.

--- Src/GojiMud/GojiMud.Engine/MudApplication.cs
+++ Src/GojiMud/GojiMud.Engine/MudApplication.cs
@@ -2,8 +2,14 @@
 {
    public class MudApplication
    {
+      private static void InitializeDependencyInjection()
+      {
+         Dependency.CreateUnityContainer();
+      }
+
       public void Run( string[] arguments )
       {
+         InitializeDependencyInjection();
       }
    }
 }
