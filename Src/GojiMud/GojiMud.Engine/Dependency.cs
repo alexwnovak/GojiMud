cafd6000b7c799b8df55af8544bd16eeec186b75
Alex Novak <alexwnovak@gmail.com>
2014-02-05 13:23:30 -0500

    +Dependency class that wraps UnityContainer.

---
+++ Src/GojiMud/GojiMud.Engine/Dependency.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace GojiMud.Engine
+{
+   public static class Dependency
+   {
+      private static IUnityContainer _unityContainer;
+
+      public static IUnityContainer UnityContainer
+      {
+         get
+         {
+            return _unityContainer;
+         }
+      }
+
+      public static void CreateUnityContainer()
+      {
+         _unityContainer = new UnityContainer();
+      }
+
+      public static void RegisterInstance<T>( T instance )
+      {
+         _unityContainer.RegisterInstance( instance );
+      }
+
+      public static void RegisterType<TFrom, TTo>() where TTo : TFrom
+      {
+         _unityContainer.RegisterType<TFrom, TTo>();
+      }
+
+      public static T Resolve<T>()
+      {
+         return _unityContainer.Resolve<T>();
+      }
+   }
+}
